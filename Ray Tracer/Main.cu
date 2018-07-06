a480362cac5a22207bd3d25df908c1917b079d0e
Peter <pes728@icloud.com>
2018-07-06 17:22:51 -0500

    reworked Clear and fixed CudaMallocManaged

--- Ray Tracer/Main.cu
+++ Ray Tracer/Main.cu
@@ -21,17 +21,17 @@ unsigned int HEIGHT = INITIALHEIGHT;
 sf::Texture screen;
 sf::Uint8* ColorBuffer;
 
-void Clear(Vec4f ClearColor) {
-	for (register int i = 0; i < WIDTH*HEIGHT * 4; i += 4) {
-			ColorBuffer[i + 0] = ClearColor.x();
-			ColorBuffer[i + 1] = ClearColor.y();
-			ColorBuffer[i + 2] = ClearColor.z();
-			ColorBuffer[i + 3] = ClearColor.w();
+__device__ void Clear(float r, float, g, float b, float a) {
+	for (int i = 0; i < WIDTH*HEIGHT * 4; i += 4) {
+			ColorBuffer[i + 0] = r;
+			ColorBuffer[i + 1] = g;
+			ColorBuffer[i + 2] = b;
+			ColorBuffer[i + 3] = a;
 	}
 }
 
 __global__ void Render(sf::Uint8 *ColorBuffer) {
-	
+	Clear(255,0,0,255);
 }
 
 int main() {
@@ -42,7 +42,9 @@ int main() {
 	mSprite.setTexture(screen);
 	sf::Event evnt;
 
-	cudaMallocManaged(&ColorBuffer, WIDTH * HEIGHT * 4);
+	ColorBuffer = new sf::Uint8[WIDTH * HEIGHT * 4];
+
+	cudaMallocManaged(&ColorBuffer, sizeof(sf::Uint8)* WIDTH * HEIGHT * 4);
 
 	Vec3f tri[3] = { Vec3f(0,0,2),Vec3f(0,200,2),Vec3f(200,0,2)};
 
@@ -76,11 +78,14 @@ int main() {
 				delete ColorBuffer;
 				ColorBuffer = nullptr;
 				ColorBuffer = new sf::Uint8[WIDTH*HEIGHT * 4];
+
+				cudafree(ColorBuffer);
+				cudaMalloc
+
 				break;
 			}
 		}
-		
-		Clear(Vec4f(255,0,0,255));
+
 		sf::Clock clock;
 		//render
 		Render<<<1,1>>>(ColorBuffer);
